5b704f7920cd29ec0d9ccc008805473de045fbef
Jason Guzik <jguzik@gmail.com>
2015-07-18 16:10:37 -0400

    fixed association problem and application controller for thank you view

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -2,6 +2,6 @@ class Survey < ActiveRecord::Base
 
   has_many :questions
   has_many :user_surveys
-  belongs_to :user, foreign_key: :owner_id
+  belongs_to :owner, class_name: 'User', foreign_key: :owner_id
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   # root 'welcome#index'
   resources :sessions, only: [:new, :create, :destroy]
   resources :surveys, :users, :questions
+  get '/thanks' => 'application#thanks'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
