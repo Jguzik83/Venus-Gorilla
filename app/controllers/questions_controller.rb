ee6a851b2eddf3d754412fd6b93f41466a0d3991
George Paul <gpaul.empyrean@gmail.com>
2015-07-18 19:28:23 -0400

    Add question update, works in database

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -13,14 +13,17 @@ class QuestionsController < ApplicationController
   def update
   puts "********************************************************************"
   params[:choices].each { |choice|
-    puts choice[0]
     new_choice = Choice.find_by_id(choice[0])
     new_choice.update_attributes(c_name: choice[1])
     new_choice.save!
-  puts "********************************************************************"
-    puts choice[1]
-  puts "********************************************************************"
   }
+  puts "********************************************************************"
+  params[:question].each do |q|
+    new_question = Question.find_by_id(q[0])
+    new_question.update_attributes(q_name: q[1])
+    new_question.save!
+  end
+  puts "********************************************************************"
   redirect_to new_question_path
   end
 
