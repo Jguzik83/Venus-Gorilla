1a7e2fbdd0bc0c7a8495820ff9cd7ea1298cdff0
George Paul <gpaul.empyrean@gmail.com>
2015-07-18 19:04:07 -0400

    Add working nested edit view, not updating db yet

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,6 +1,22 @@
 class QuestionsController < ApplicationController
-  def show
+  # def show
+  #   @question = Question.find_by_id(params[:id])
+  # end
+
+  def new
+  end
+
+  def edit
     @question = Question.find_by_id(params[:id])
+  end
+
+  def update
+  puts "********************************************************************"
+  params["choices"].each { |choice|
+    puts choice[1]
+  }
+  puts "********************************************************************"
+  return 0
   end
 
 end
