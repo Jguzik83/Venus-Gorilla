67f30e3518463824dbe94aa8bb6f514225920fac
Jonathan Chung <hukashi@gmail.com>
2015-07-18 11:03:17 -0400

    add routes for index, new, create and views for index, new, show

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -1,2 +1,36 @@
 class SurveysController < ApplicationController
+
+  def index
+    @user = User.find_by(id: session[:user_id])
+    @surveys = Survey.all
+  end
+
+  def show
+    @user = User.find_by(id: session[:user_id])
+    @survey = Survey.find_by(id: params[:id])
+    @questions = Questions.where(survey_id: @survey.id)
+  end
+
+  def new
+    @user = @user = User.find_by(id: session[:user_id])
+    @survey = Survey.new
+  end
+
+  def create
+    user = User.find_by(id: session[:user_id])
+    survey = Survey.new(survey_params)
+    survey.user = user
+    survey.save
+    redirect_to
+  end
+
+  def edit
+  end
+
+  private
+
+  def survey_params
+    params.require(:survey).permit(:title, :description, :user_id)
+  end
+
 end
