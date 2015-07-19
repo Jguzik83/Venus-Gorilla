bcb2c8320dc87d759b5134e9a4f0a46dcb3ef1b1
Jonathan Chung <hukashi@gmail.com>
2015-07-18 21:53:49 -0400

    fixed sign up page and redirect logic and authentication fixes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,14 +3,13 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-    def logged_in?
+    def require_login
+      redirect_to root_path unless is_authenticated?
+        # flash[:notice] = "You must be logged in to view this page."
+    end
+
+    def is_authenticated?
       !!session[:user_id]
     end
 
-    def require_login
-      unless logged_in?
-        flash[:notice] = "You must be logged in to view this page."
-        redirect_to sessions_path
-      end
-    end
 end
