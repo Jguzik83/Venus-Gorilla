2b749bffc8bf93975c2f376e9267b34322a3fde4
Jonathan Chung <hukashi@gmail.com>
2015-07-20 19:09:12 -0400

    add fix for ajax on question

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,12 +1,12 @@
 $(document).ready(function() {
-  $('#question_form').on('submit', function(event){
+  $('#form-partial').on('submit', function(event){
     event.preventDefault();
     $.ajax({
       url: event.target.action,
       method: event.target.method,
       data: $(event.target).serialize()
     }).success(function(response){
-      console.log(response)
+      $('#form-partial').html(response);
     }).fail(function(error){
       console.log(error)
     });
--- app/controllers/survey_choices_controller.rb
+++ app/controllers/survey_choices_controller.rb
@@ -3,13 +3,29 @@ class SurveyChoicesController < ApplicationController
   def create
     survey_choice = SurveyChoice.new(survey_choice_params)
     # survey_choice.save!
+    @user_survey = UserSurvey.find_by(id: survey_choice.user_survey_id)
+    @question = @user_survey.give_question
     if survey_choice.save && request.xhr?
-      @user_survey = UserServey.find_by(id: survey_choice.user_survey_id)
-      @question = @user_survey.give_question
-      'render "show", locals: {question: @question}'
+      render(partial: 'user_surveys/show', layout: false, locals:{question: @question, user_survey: @user_survey})
+      #'render "show", locals: {question: @question}'
+    else
+      redirect_to root_path
     end
     # redirect_to user_survey_path(survey_choice.user_survey_id)
   end
+  # def create
+  #   survey_choice = SurveyChoice.new(survey_choice_params)
+  #   # survey_choice.save!
+  #   if survey_choice.save && request.xhr?
+  #     @user_survey = UserSurvey.find_by(id: survey_choice.user_survey_id)
+  #     @question = @user_survey.give_question
+  #     request.xhr? ? render(partial: 'user_surveys/show', layout: false, locals:{question: @question, user_survey: @user_survey}) : redirect_to(@question)
+  #     #'render "show", locals: {question: @question}'
+  #   else
+  #     redirect_to root_path
+  #   end
+  #   # redirect_to user_survey_path(survey_choice.user_survey_id)
+  # end
 
   private
 
